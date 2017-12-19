ec6ed7e0de8ff2e3fe665528739b1b43da8cf9d7
DAlekhin <Scav@outlook.com>
2017-12-20 00:16:51 +0300

    add new datapoint

--- ADV.InternetCrawler.ControlPanel/Controllers/DataPointController.cs
+++ ADV.InternetCrawler.ControlPanel/Controllers/DataPointController.cs
@@ -6,14 +6,100 @@ using System.Web.Mvc;
 using ADV.InternetCrawler.ControlPanel.Models;
 using ADV.InternetCrawler.Models;
 using System.Web.Script.Serialization;
+using System.Text.RegularExpressions;
 
 namespace ADV.InternetCrawler.ControlPanel.Controllers
 {
     public class DataPointController : Controller
     {
-        public ActionResult NewDataPoint()
+        [HttpGet]
+        public ActionResult EditDataPoint(int? ID)
         {
-            return View();
+            DataPointEditModel l_model = null;
+
+            try
+            {
+                if (ID != null)
+                {
+                    DataPoint l_dataPoint = null;
+                    int l_id = ID.GetValueOrDefault();
+
+                    GetDataFromPoint l_detDataPoint = new GetDataFromPoint();
+
+                    l_dataPoint = l_detDataPoint.GetDataPoint(l_id);
+
+                    l_model = new DataPointEditModel
+                    {
+                        ID = l_dataPoint.ID,
+                        Name = l_dataPoint.Name,
+                        URI = l_dataPoint.Uri,
+
+                        PageURIStart = SeparateRegex(l_dataPoint.PageUri, "Start"),
+                        PageURIData = SeparateRegex(l_dataPoint.PageUri, "Data"),
+                        PageURIFinish = SeparateRegex(l_dataPoint.PageUri, "Finish"),
+
+                        ItemURIStart = SeparateRegex(l_dataPoint.ItemUri, "Start"),
+                        ItemURIData = SeparateRegex(l_dataPoint.ItemUri, "Data"),
+                        ItemURIFinish = SeparateRegex(l_dataPoint.ItemUri, "Finish"),
+
+                        ItemNameStart = SeparateRegex(l_dataPoint.ItemName, "Start"),
+                        ItemNameData = SeparateRegex(l_dataPoint.ItemName, "Data"),
+                        ItemNameFinish = SeparateRegex(l_dataPoint.ItemName, "Finish"),
+
+                        ItemPriceStart = SeparateRegex(l_dataPoint.ItemPrice, "Start"),
+                        ItemPriceData = SeparateRegex(l_dataPoint.ItemPrice, "Data"),
+                        ItemPriceFinish = SeparateRegex(l_dataPoint.ItemPrice, "Finish"),
+
+                        ItemDiscountPriceStart = SeparateRegex(l_dataPoint.ItemPictureUri, "Start"),
+                        ItemDiscountPriceData = SeparateRegex(l_dataPoint.ItemPictureUri, "Data"),
+                        ItemDiscountPriceFinish = SeparateRegex(l_dataPoint.ItemPictureUri, "Finish"),
+
+                        ItemArticleStart = SeparateRegex(l_dataPoint.ItemArticle, "Start"),
+                        ItemArticleData = SeparateRegex(l_dataPoint.ItemArticle, "Data"),
+                        ItemArticleFinish = SeparateRegex(l_dataPoint.ItemArticle, "Finish"),
+
+                        ItemPictureUriStart = SeparateRegex(l_dataPoint.ItemPictureUri, "Start"),
+                        ItemPictureUriData = SeparateRegex(l_dataPoint.ItemPictureUri, "Data"),
+                        ItemPictureUriFinish = SeparateRegex(l_dataPoint.ItemPictureUri, "Finish")
+                    };
+                }
+            }
+            catch
+            {
+
+            }
+
+            return View(l_model);
+        }
+
+        [HttpPost]
+        public JsonResult EditDataPoint(DataPointEditModel Model)
+        {
+            try
+            {
+                DataPoint l_dataPoint = new DataPoint
+                {
+                    ID = Model.ID ?? 0,
+                    Name = Model.Name,
+                    Uri = Model.URI,
+
+                    PageUri = Model.PageURIStart + Model.PageURIData + Model.PageURIFinish,
+                    ItemUri = Model.ItemURIStart + Model.ItemURIData + Model.ItemURIFinish,
+                    ItemName = Model.ItemNameStart + Model.ItemNameData + Model.ItemNameFinish,
+                    ItemPrice = Model.ItemPriceStart + Model.ItemPriceData + Model.ItemPriceFinish,
+                    ItemDiscountPrice = Model.ItemDiscountPriceStart + Model.ItemDiscountPriceData + Model.ItemDiscountPriceFinish,
+                    ItemArticle = Model.ItemArticleStart + Model.ItemArticleData + Model.ItemArticleFinish,
+                    ItemPictureUri = Model.ItemDiscountPriceStart + Model.ItemDiscountPriceData + Model.ItemDiscountPriceFinish
+                };
+
+                l_dataPoint.Save();
+            }
+            catch (Exception l_exc)
+            {
+                return Json(new { Status = false, Error = l_exc.Message });
+            }
+
+            return Json(new { Status = true });
         }
 
         public ActionResult ShowDataPoint()
@@ -41,6 +127,25 @@ namespace ADV.InternetCrawler.ControlPanel.Controllers
             }
 
             return new LargeJsonResult() { Data = l_dataPointStats };
+        }
+
+        private String SeparateRegex(String _regex, String _partName)
+        {
+            String l_separatedPart = null;
+
+            try
+            {
+                Regex l_regex = new Regex(@"(?<Start>.*)\(\?<Data>(?<Data>[^\)]+)\)(?<Finish>.*)");
+                Match l_regexMatch = l_regex.Match(_regex);
+
+                l_separatedPart = l_regexMatch.Groups[_partName].Value;
+            }
+            catch
+            {
+
+            }
+
+            return l_separatedPart;
         }
     }
 }
--- ADV.InternetCrawler.ControlPanel/Models/DataPointModels.cs
+++ ADV.InternetCrawler.ControlPanel/Models/DataPointModels.cs
@@ -4,5 +4,50 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ADV.InternetCrawler.ControlPanel.Models
 {
+    public class DataPointEditModel
+    {
+        [Display(Name = "#")]
+        public Int32? ID { get; set; }
 
+        [Display(Name = "Наименование")]
+        public String Name { get; set; }
+
+        [Display(Name = "Стартовая ссылка")]
+        public String URI { get; set; }
+
+        [Display(Name = "Regex ссылка на страницу")]
+        public String PageURIStart { get; set; }
+        public String PageURIData { get; set; }
+        public String PageURIFinish { get; set; }
+
+        [Display(Name = "Regex ссылка на товар")]
+        public String ItemURIStart { get; set; }
+        public String ItemURIData { get; set; }
+        public String ItemURIFinish { get; set; }
+
+        [Display(Name = "Regex название товара")]
+        public String ItemNameStart { get; set; }
+        public String ItemNameData { get; set; }
+        public String ItemNameFinish { get; set; }
+
+        [Display(Name = "Regex цена товара")]
+        public String ItemPriceStart { get; set; }
+        public String ItemPriceData { get; set; }
+        public String ItemPriceFinish { get; set; }
+
+        [Display(Name = "Regex цена со скидкой")]
+        public String ItemDiscountPriceStart { get; set; }
+        public String ItemDiscountPriceData { get; set; }
+        public String ItemDiscountPriceFinish { get; set; }
+
+        [Display(Name = "Regex артикул товара")]
+        public String ItemArticleStart { get; set; }
+        public String ItemArticleData { get; set; }
+        public String ItemArticleFinish { get; set; }
+
+        [Display(Name = "Regex ссылка на изображение товара")]
+        public String ItemPictureUriStart { get; set; }
+        public String ItemPictureUriData { get; set; }
+        public String ItemPictureUriFinish { get; set; }
+    }
 }
